9bfd5339161ded52e698290fd7e2b2c9da96287d
liuchunhua <thumb0422@163.com>
2018-07-01 22:05:55 +0800

    update

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,66 +1,57 @@
 // pages/detail/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
-  },
+    isLike: true,
+    // banner
+    imgUrls: [
+      "http://mz.djmall.xmisp.cn/files/product/20161201/148057921620_middle.jpg",
+      "http://mz.djmall.xmisp.cn/files/product/20161201/148057922659_middle.jpg",
+      "http://mz.djmall.xmisp.cn/files/product/20161201/148057923813_middle.jpg",
+      "http://mz.djmall.xmisp.cn/files/product/20161201/148057924965_middle.jpg",
+      "http://mz.djmall.xmisp.cn/files/product/20161201/148057925958_middle.jpg"
+    ],
+    indicatorDots: true, //是否显示面板指示点
+    autoplay: true, //是否自动切换
+    interval: 3000, //自动切换时间间隔,3s
+    duration: 1000, //  滑动动画时长1s
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+    // 商品详情介绍
+    detailImg: [
+      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_1.jpg",
+      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_2.jpg",
+      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_3.jpg",
+      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_4.jpg",
+      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_5.jpg",
+      "http://7xnmrr.com1.z0.glb.clouddn.com/detail_6.jpg",
+    ],
   },
+  //预览图片
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.imgUrls // 需要预览的图片http链接列表  
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  // 收藏
+  addLike() {
+    this.setData({
+      isLike: !this.data.isLike
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  // 跳到购物车
+  toCar() {
+    wx.switchTab({
+      url: '/pages/cart/cart'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  // 立即购买
+  immeBuy() {
+    wx.showToast({
+      title: '购买成功',
+      icon: 'success',
+      duration: 2000
+    });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
--- pages/list/index.js
+++ pages/list/index.js
@@ -1,231 +1,132 @@
 // pages/list/index.js
 var util = require('../../utils/util.js')
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    currentIndex:-1,
-    leftDatas: [],
-    rightDatas: [],
-    rightFilterDatas: [],
+    cateItems: [
+      {
+        cate_id: 1,
+        cate_name: "护肤",
+        ishaveChild: true,
+        children:
+        [
+          {
+            child_id: 1,
+            name: '洁面皂',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117972563.jpg"
+          },
+          {
+            child_id: 2,
+            name: '卸妆',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161207/148110444480.jpg"
+          },
+          {
+            child_id: 3,
+            name: '洁面乳',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117973270.jpg"
+          },
+          {
+            child_id: 4,
+            name: '面部祛角质',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117981591.jpg"
+          }
+        ]
+      },
+      {
+        cate_id: 2,
+        cate_name: "彩妆",
+        ishaveChild: true,
+        children:
+        [
+          {
+            child_id: 1,
+            name: '气垫bb',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381301.jpg"
+          },
+          {
+            child_id: 2,
+            name: '修容/高光',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381411.jpg"
+          },
+          {
+            child_id: 3,
+            name: '遮瑕',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153815181.jpg"
+          },
+          {
+            child_id: 4,
+            name: '腮红',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153815759.jpg"
+          },
+          {
+            child_id: 5,
+            name: '粉饼',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153816983.jpg"
+          },
+          {
+            child_id: 6,
+            name: '粉底',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153817721.jpg"
+          },
+          {
+            child_id: 7,
+            name: '蜜粉/散粉',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161212/148153819354.jpg"
+          },
+          {
+            child_id: 8,
+            name: '隔离霜',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161215/148179053369.jpg"
+          }
+        ]
+      },
+      {
+        cate_id: 3,
+        cate_name: "香水/香氛",
+        ishaveChild: true,
+        children:
+        [
+          {
+            child_id: 1,
+            name: '淡香水EDT',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161213/14815978910.jpg"
+          },
+          {
+            child_id: 2,
+            name: '浓香水EDP',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161213/148159789883.jpg"
+          },
+          {
+            child_id: 3,
+            name: '香体走珠',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161213/14815979307.jpg"
+          },
+          {
+            child_id: 4,
+            name: '古龙香水男士的最爱',
+            image: "http://mz.djmall.xmisp.cn/files/logo/20161213/148159765589.jpg"
+          }
+        ]
+      },
+      {
+        cate_id: 4,
+        cate_name: "个人护理",
+        ishaveChild: false,
+        children: []
+      }
+    ],
+    curNav: 1,
+    curIndex: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.requestMockData()
-    console.log('-------',options)
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  /**
-   * 生成测试数据 
-   */
-  requestMockData: function () {
+  //事件处理函数  
+  switchRightTab: function (e) {
+    // 获取item项的id，和数组的下标值  
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    // 把点击到的某一项，设为当前index  
     this.setData({
-      leftDatas: [{
-        id: "0001",
-        title: "春季款",
-        imgUrl: "http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"
-      },
-      {
-        id: "0002",
-        title: "夏季款",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0003",
-        title: "秋季款",
-        imgUrl: "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
-      },
-      {
-        id: "0004",
-        title: "冬季款",
-        imgUrl: "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg"
-      },
-      {
-        id: "0005",
-        title: "秋冬季款5",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0006",
-        title: "秋冬季款6",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0007",
-        title: "秋冬季款7",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0008",
-        title: "秋冬季款8",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0009",
-        title: "秋冬季款9",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0010",
-        title: "秋冬季款10",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      {
-        id: "0011",
-        title: "秋冬季款11",
-        imgUrl: "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
-      },
-      ],
-
-      rightDatas: [{
-        id: "0001",
-        title: "春季款001",
-      },
-      {
-        id: "0001",
-        title: "春季款002",
-      },
-      {
-        id: "0003",
-        title: "春季款003",
-      },
-      {
-        id: "0003",
-        title: "春季款004",
-      },
-      {
-        id: "0003",
-        title: "春季款005",
-      },
-      {
-        id: "0005",
-        title: "春季款006",
-      },
-      {
-        id: "0006",
-        title: "春季款007",
-      },
-      {
-        id: "0006",
-        title: "春季款008",
-      },
-      {
-        id: "0006",
-        title: "春季款009",
-      },
-      {
-        id: "0006",
-        title: "春季款010",
-      },
-      {
-        id: "0006",
-        title: "春季款011",
-      },
-      {
-        id: "0006",
-        title: "春季款012",
-      },
-      {
-        id: "0006",
-        title: "春季款013",
-      },
-      {
-        id: "0009",
-        title: "春季款014",
-      },
-      {
-        id: "0009",
-        title: "春季款015",
-      },
-      {
-        id: "0009",
-        title: "春季款016",
-      },
-      {
-        id: "0009",
-        title: "春季款017",
-      },
-      {
-        id: "0009",
-        title: "春季款018",
-      },
-      {
-        id: "0009",
-        title: "春季款019",
-      },
-      {
-        id: "0009",
-        title: "春季款020",
-      },
-      {
-        id: "0009",
-        title: "春季款021",
-      },
-      ]
+      curNav: id,
+      curIndex: index
     })
-  },
-  
-  leftClick: function (e) {
-    var that = this
-    let leftDataId = e.currentTarget.dataset['id']
-    let filterData = util.JsonArrayFilter(that.data.rightDatas, leftDataId)
-    that.setData({
-      rightFilterDatas: filterData
-    })
-  },
-
+  }
 })
